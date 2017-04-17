339ee96ed052630b26bd1ec3d5d490f42bc49459
Shilpa Dutta <dutta.shilpa6@gmail.com>
2017-04-16 21:06:23 -0700

    Delete code.py

--- prework/code.py
+++
@@ -1,6 +0,0 @@
-name = raw_input("What is your name?")
-lname = raw_input("What is your last name?")
-color = raw_input("What is your favorite color?")
-pets = raw_input("How many pets do you have?")
-print "Great! so your name is %s %s, your favorite color is %s, and you have %s pets. Nice to meet you!" % (name, lname, color, pets)
-
